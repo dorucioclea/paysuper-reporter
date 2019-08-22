9c19cd73bf9c2fde93c577083d1e29deb391a87d
Vadim Sabirov <pr0head@gmail.com>
2019-08-22 10:20:23 +0300

    PAY-242 #190373 Add configuration parameters for the Centrifugo, mongo mode and document retention time

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,15 @@
 package config
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	"github.com/globalsign/mgo"
+	"github.com/kelseyhightower/envconfig"
+)
 
 // MongoConfig defines the parameters for connecting to the MongoDB.
 type MongoConfig struct {
-	Dsn         string `envconfig:"MONGO_DSN" required:"true"`
-	DialTimeout string `envconfig:"MONGO_DIAL_TIMEOUT" required:"false" default:"10"`
+	Dsn         string   `envconfig:"MONGO_DSN" required:"true"`
+	DialTimeout string   `envconfig:"MONGO_DIAL_TIMEOUT" required:"false" default:"10"`
+	MongoMode   mgo.Mode `envconfig:"MONGO_MODE" required:"false" default:"4"`
 }
 
 // NatsConfig defines the parameters for connecting to the NATS streaming server.
@@ -28,20 +32,30 @@ type S3Config struct {
 	Secure      bool   `envconfig:"S3_SECURE" default:"false"`
 }
 
-// DocumentGenerator defines the parameters for connecting to the document generator service.
-type DocumentGenerator struct {
-	ApiUrl  string `envconfig:"DOC_API_URL" default:"http://127.0.0.1:5488"`
-	Timeout int    `envconfig:"DOC_API_TIMEOUT" default:"60000"`
+// Centrifugo defines the parameters for connecting to the Centrifugo server.
+type CentrifugoConfig struct {
+	ApiSecret       string `envconfig:"CENTRIFUGO_API_SECRET" required:"true"`
+	Secret          string `envconfig:"CENTRIFUGO_SECRET" required:"true"`
+	URL             string `envconfig:"CENTRIFUGO_URL" required:"false" default:"http://127.0.0.1:8000"`
+	MerchantChannel string `envconfig:"CENTRIFUGO_MERCHANT_CHANNEL" default:"paysuper:merchant#%s"`
+}
+
+// DocumentGeneratorConfig defines the parameters for connecting to the document generator service.
+type DocumentGeneratorConfig struct {
+	ApiUrl  string `envconfig:"DOCGEN_API_URL" default:"http://127.0.0.1:5488"`
+	Timeout int    `envconfig:"DOCGEN_API_TIMEOUT" default:"60000"`
 }
 
 type Config struct {
-	Db   MongoConfig
-	Nats NatsConfig
-	S3   S3Config
-	DG   DocumentGenerator
+	Db               MongoConfig
+	Nats             NatsConfig
+	S3               S3Config
+	DG               DocumentGeneratorConfig
+	CentrifugoConfig CentrifugoConfig
 
-	MicroRegistry      string `envconfig:"MICRO_REGISTRY" required:"false"`
-	DocumentStorageTtl string `envconfig:"DOCUMENT_STORAGE_TTL" default:"604800"`
+	MetricsPort           string `envconfig:"METRICS_PORT" required:"false" default:"8086"`
+	MicroRegistry         string `envconfig:"MICRO_REGISTRY" required:"false"`
+	DocumentRetentionTime string `envconfig:"DOCUMENT_RETENTION_TIME" default:"604800"`
 }
 
 func NewConfig() (*Config, error) {
